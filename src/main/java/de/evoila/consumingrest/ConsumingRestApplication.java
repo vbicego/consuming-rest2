815aec59b7abba10c205a167933e73a5188e8732
Victor Bicego <vbicego@evoila.de>
2022-01-08 21:13:21 +0100

    Implements ComandLineRunner and simplify the restTemplate and log

--- src/main/java/de/evoila/consumingrest/ConsumingRestApplication.java
+++ src/main/java/de/evoila/consumingrest/ConsumingRestApplication.java
@@ -5,12 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class ConsumingRestApplication {
+public class ConsumingRestApplication implements CommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(ConsumingRestApplication.class);
 
@@ -18,18 +16,25 @@ public class ConsumingRestApplication {
         SpringApplication.run(ConsumingRestApplication.class, args);
     }
 
-    @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
+    @Override
+    public void run(String... args) throws Exception {
+        Todo todoToPrint = new RestTemplate().getForObject("https://jsonplaceholder.typicode.com/todos/5", Todo.class);
+        log.info(todoToPrint.toString());
     }
 
-    @Bean
-    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-        return args -> {
-            Todo todo = restTemplate.getForObject(
-                    "https://jsonplaceholder.typicode.com/todos/5", Todo.class);
-            log.info(todo.toString());
-        };
-    }
+//    @Bean
+//    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+//        return builder.build();
+//    }
+
+//    @Bean
+//    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+//        return args -> {
+//            Todo todo = restTemplate.getForObject(
+//                    "https://jsonplaceholder.typicode.com/todos/5", Todo.class);
+//            log.info(todo.toString());
+//        };
+//    }
+
 
 }
