85d3acbb50d2df5fdb0a74fbeedeb537ca52694a
Victor Bicego <vbicego@evoila.de>
2022-01-08 13:12:53 +0100

    Add Todo, Logger, RestTemplate and CommandLineRunner

---
+++ src/main/java/de/evoila/consumingrest/Todo.java
@@ -0,0 +1,57 @@
+package de.evoila.consumingrest;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Todo {
+
+    private Long userId;
+    private Long id;
+    private String title;
+    private Boolean completed;
+
+    public Todo() {
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+
+    @Override
+    public String toString() {
+        return "Todo{" +
+                "userId=" + userId +
+                ", id=" + id +
+                ", title='" + title + '\'' +
+                ", completed=" + completed +
+                '}';
+    }
+}
