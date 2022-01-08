7b5fe54e2b2a4ddbe596f0c602d29606c1ac2c4d
Victor Bicego <vbicego@evoila.de>
2022-01-08 13:02:03 +0100

    First commit

---
+++ src/main/java/de/evoila/consumingrest/ConsumingrestApplication.java
@@ -0,0 +1,13 @@
+package de.evoila.consumingrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingrestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/evoila/consumingrest/ConsumingrestApplicationTests.java
@@ -0,0 +1,13 @@
+package de.evoila.consumingrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
